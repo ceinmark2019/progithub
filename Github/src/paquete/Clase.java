ac8616a637fb1fc6137ab44c9a9f026a7c319093
Gerar <Gerar@Ordenador>
2019-04-28 18:16:00 +0200

    versión 1.1 Saludo desde Netbeans

--- Github/src/paquete/Clase.java
+++ Github/src/paquete/Clase.java
@@ -6,6 +6,6 @@ public class Clase {
 	public static void main (String[] args) {
 		
 		System.out.println("hola Github");
-                System.out.println("Hola Eclipse desde Netbeans");
+                System.out.println("Hola Eclipse desde Netbeans1");
 	}
 }
