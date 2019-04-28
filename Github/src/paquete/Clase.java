277b3f78942a6b0daeef8e658553477518843af9
daw <daw@192.168.0.201>
2019-04-28 19:06:43 +0200

    Saludo desde eclipse

--- Github/src/paquete/Clase.java
+++ Github/src/paquete/Clase.java
@@ -6,6 +6,6 @@ public class Clase {
 	public static void main (String[] args) {
 		
 		System.out.println("hola Github");
-		System.out.println("Hola desde Eclipse");
+		System.out.println("Un saludo desde Eclipse");
 	}
 }
