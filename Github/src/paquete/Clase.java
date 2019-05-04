7fabccf158c794e7ce09d95a13a9e510682a4166
daw <daw@192.168.0.201>
2019-05-04 15:51:43 +0200

    Corrección de Errores

--- Github/src/paquete/Clase.java
+++ Github/src/paquete/Clase.java
@@ -6,11 +6,11 @@ public class Clase {
 	public static void main (String[] args) {
 		
 		System.out.println("hola Github");
-<<<<<<< HEAD
+
 		System.out.println("Un saludo desde Eclipse 1");
-=======
+
                 System.out.println("hola desde Netbeans");
                               
->>>>>>> branch 'master' of https://github.com/ceinmark2019/progithub.git
+
 	}
 }
