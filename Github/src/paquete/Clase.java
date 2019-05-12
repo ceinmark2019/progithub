307fe3d6eb6dce0e8c448d8162b30c451ea30278
daw <daw@192.168.0.201>
2019-05-12 12:55:22 +0200

    Version final

--- Github/src/paquete/Clase.java
+++ Github/src/paquete/Clase.java
@@ -5,8 +5,7 @@ public class Clase {
 	
 	public static void main (String[] args) {
 		
-		System.out.println("hola Github");
-        System.out.println("adios Github");               
+		System.out.println("fin del proyecto");               
 
 	}
 }
