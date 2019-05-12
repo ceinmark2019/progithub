046caca929cf969c4b9fb8f5d9ed9759fba1a50e
daw <daw@192.168.0.201>
2019-05-12 12:38:09 +0200

    Version 2.0

--- Github/src/paquete/Clase.java
+++ Github/src/paquete/Clase.java
@@ -6,7 +6,7 @@ public class Clase {
 	public static void main (String[] args) {
 		
 		System.out.println("hola Github");
-                       
+        System.out.println("adios Github");               
 
 	}
 }
